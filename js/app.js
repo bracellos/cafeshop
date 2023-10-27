e554da920a035a76d1b1be6233d99f7e1224ee1e
Diego Bracellos <bracellos@gmail.com>
2023-10-27 20:08:34 -0300

    pegando itens do json

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
 $(function(){
-    let estrutura;
+    let estrutura = '';
 
     let loadProdutos = () => {
         $.ajax({
             url : "/produtos.json",
             dataType : "JSON",
-            success : function(produtos){
-                //fazer loop nos produtos
-                for(pos in produtos){
+            success : function(data){
+                //fazer loop nos data
+                console.log(data.produtos)
+                for(pos in data.produtos){
                     // estrutura = estrutura + `<div></div>`
                     estrutura += `
                     <div class="col-sm-12 col-md-3">
                     <div class="card">
-                        <img src="img/${produtos[pos].img}" class="card-img-top" alt="...">
+                        <img src="img/${data.produtos[pos].img}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">${produtos[pos].title}</h5>
-                            <p class="card-text">${produtos[pos].description}</p>
+                            <h5 class="card-title">${data.produtos[pos].title}</h5>
+                            <p class="card-text">${data.produtos[pos].description}</p>
                             <a href="#" class="btn btn-success">COMPRAR</a>
-                            <span class="badge bg-danger fs-6">R$ ${produtos[pos].price}</span>
+                            <span class="badge bg-danger fs-6">R$ ${data.produtos[pos].price}</span>
                         </div>
                     </div>
                     </div>
                     `;
+                    
                 }
+
+                //injetar o conteudo no index
+                $('#loadProdutos').html(estrutura)
             }
         })
     }
